92e78ea93cb5903dfccc38dee82e1be33f36763c
kevinlee584 <kevinlee584@gmail.com>
2018-11-28 14:22:17 +0800

    add: gamepage

---
+++ src/calculatorplay/GamePageController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication3;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class GamePageController implements Initializable {
+
+    
+    @FXML
+    private void BackButton(ActionEvent event) throws IOException, URISyntaxException {
+            String value = ((Button)event.getSource()).getText();
+            Parent root = FXMLLoader.load(getClass().getResource("SelectPage.fxml"));
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+    }   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
